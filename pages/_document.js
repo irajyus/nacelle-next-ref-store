7444c36a4d86cc0ab0b9121e4765042dcf61e61c
irajyus <irach.yusufiy@getnacelle.com>
2022-06-15 13:54:15 -0400

    implement middleware & gtm

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,6 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { GTM_ID } from '../lib/gtm';
+
 class MyDocument extends Document {
   render() {
     return (
@@ -14,6 +16,14 @@ class MyDocument extends Document {
           />
         </Head>
         <body>
+          <noscript>
+            <iframe
+              src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+              height="0"
+              width="0"
+              style={{ display: 'none', visibility: 'hidden' }}
+            />
+          </noscript>
           <Main />
           <NextScript />
         </body>
